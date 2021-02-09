51aa4ff2f0b8ba020b2a568421c1f2fbda5256a2
lakshmi87ambati <lt.ambati@gmail.com>
2021-02-09 02:40:24 +0000

    FIRST COMMIT

---
+++ com.feb9.AppTest/src/test/java/com/App/Test/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.App.Test;
+
+
+import org.junit.Test;
+
+
+public class HelloControllerTest {
+    
+
+    @Test
+    public void testHello() throws Exception {
+       System.out.println("selenium testing");
+    }
+
+    
+}
